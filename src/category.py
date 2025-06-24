c5db5ba815466e7f84540155f917227c59551a07
Andrey Sapeshko <andrdd17@gmail.com>
2025-06-24 20:10:17 +0300

    в модуле category добавлен метод add_product и геттер products

--- src/category.py
+++ src/category.py
@@ -4,13 +4,24 @@ from src.product import Product
 class Category:
     name: str
     description: str
-    products: list[Product]
+    _products: list[Product]
     category_count = 0
     product_count = 0
 
     def __init__(self, name: str, description: str, products: list[Product]=None) -> None:
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self._products = products if products else []
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self._products)
+
+
+    def add_product(self, product: Product):
+        self._products.append(product)
+
+
+    @property
+    def products(self):
+        for product in self._products:
+            print(f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.')
+        return self._products
