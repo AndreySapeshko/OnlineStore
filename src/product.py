792aac20bccb4790ad0cdb1f2b2cc3a6858fac17
Andrey Sapeshko <andrdd17@gmail.com>
2025-06-25 10:52:13 +0300

    в модуле product добавлена функция new_product и тест к ней

--- src/product.py
+++ src/product.py
@@ -9,3 +9,20 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+
+    @classmethod
+    def new_product(cls, params: list) -> None:
+        try:
+            name = params[0]
+            description = params[1]
+            price = params[2]
+            quantity = params[3]
+        except Exception('Количество или тип параметров не соответстует ожиданию') as e:
+            print(f'Ошибка: {e}')
+        return Product(name, description, price, quantity)
+
+
+    @staticmethod
+    def check_product_matching(products: list) -> None:
+        pass
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.product import Product
 
 
@@ -6,3 +8,17 @@ def test_product(product: Product) -> None:
     assert product.description == '256GB, Серый цвет, 200MP камера'
     assert product.price == 180000.0
     assert product.quantity == 5
+
+
+def test_new_product() -> None:
+    product = Product.new_product(['Xiaomi Redmi Note 11', '1024GB, Синий', 31000.0, 14])
+    assert product.name == 'Xiaomi Redmi Note 11'
+    assert product.description == '1024GB, Синий'
+    assert product.price == 31000.0
+    assert product.quantity == 14
+
+
+def test_new_product_except() -> None:
+    with pytest.raises(Exception) as exc_info:
+        product = Product.new_product(['Xiaomi Redmi Note 11', '1024GB, Синий'])
+        assert str(exc_info.value) == 'Количество или тип параметров не соответстует ожиданию'
