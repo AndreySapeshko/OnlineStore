a11490591a21bd7ab1fa8f17012a717348bd6036
Andrey Sapeshko <andrdd17@gmail.com>
2025-07-05 09:39:15 +0300

    Изменен тест для класса IteratorCategory

--- tests/test_iterator_category.py
+++ tests/test_iterator_category.py
@@ -4,6 +4,7 @@ from src.category import Category
 
 def test_iterator_categories(category: Category) -> None:
     count_products = 0
-    for product in category.products:
+    iterator_category = IteratorCategory(category)
+    for product in iterator_category:
         count_products += 1
-    assert count_products == len(category.products)
+    assert count_products == 2
