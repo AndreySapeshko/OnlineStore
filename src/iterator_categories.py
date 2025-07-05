4984157f4feae425a148d8f207e3fa5e2341116d
Andrey Sapeshko <andrdd17@gmail.com>
2025-07-05 09:08:30 +0300

    В модуле iterator_categories создан класс IteratorCategories

---
+++ src/iterator_categories.py
@@ -0,0 +1,19 @@
+from src.category import Category
+
+class IteratorCategories:
+    category: Category
+    index_category: int = 0
+
+    def __init__(self, category) -> None:
+        self.category = category
+
+    def __iter__(self):
+        self.current_value = -1
+        return self
+
+    def __next__(self):
+        if self.current_value + 1 < len(self.category.products):
+            self.current_value += 1
+            return self.category.products[self.current_value]
+        else:
+            raise StopIteration
