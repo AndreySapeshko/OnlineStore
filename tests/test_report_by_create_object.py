1719e780d71e5c7af029a2b198036cedb88d5c79
Andrey Sapeshko <andrdd17@gmail.com>
2025-07-13 08:14:29 +0300

    В модуле test_report_by_create_object.py создан тест к  классу ReportByCreateObject

---
+++ tests/test_report_by_create_object.py
@@ -0,0 +1,18 @@
+from pytest import CaptureFixture
+
+from src.report_by_create_object import ReportByCreateObject
+
+
+class TestClass(ReportByCreateObject):
+
+    def __init__(self, arg1, arg2, arg3) -> None:
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.arg3 = arg3
+        super().__init__()
+
+
+def test_report_by_create_object(capsys: CaptureFixture[str]) -> None:
+    TestClass('One', 'Two', 'Three')
+    captured = capsys.readouterr()
+    assert captured.out == 'TestClass(One, Two, Three)\n'
