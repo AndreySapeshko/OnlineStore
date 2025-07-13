8469e2c95b62004f524febf42ff3041b1067982a
Andrey Sapeshko <andrdd17@gmail.com>
2025-07-13 09:51:41 +0300

    В модуле test_receiver создан тест к  классу Receiver

--- src/receiver.py
+++ src/receiver.py
@@ -3,6 +3,8 @@ from src.product import Product
 
 
 class Receiver(ABC):
+    """ Абстрактный класс задающий создание метода добовление
+    продуктов для объектос содержащих список продуктов """
 
     @abstractmethod
     def add_product(self, product: Product) -> None:
