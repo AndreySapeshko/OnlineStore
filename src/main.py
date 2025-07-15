391c2f85568a9bcbaf631a3a1d1c36f748b61ccc
Andrey Sapeshko <andrdd17@gmail.com>
2025-07-15 09:29:25 +0300

    Замена дублирования кода в main

--- src/main.py
+++ src/main.py
@@ -6,24 +6,17 @@ if __name__ == '__main__':
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
-    print(product1.name)
-    print(product1.description)
-    print(product1.price)
-    print(product1.quantity)
+    [print(arg) for arg in list(product1.__dict__.values())]
 
-    print(product2.name)
-    print(product2.description)
-    print(product2.price)
-    print(product2.quantity)
+    [print(arg) for arg in list(product2.__dict__.values())]
 
-    print(product3.name)
-    print(product3.description)
-    print(product3.price)
-    print(product3.quantity)
+    [print(arg) for arg in list(product3.__dict__.values())]
 
     category1 = Category("Смартфоны",
                          "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
                          [product1, product2, product3])
+
+    map(print, [arg for arg in list(category1.__dict__) if not isinstance(arg, list)])
 
     print(category1.name == "Смартфоны")
     print(category1.description)
